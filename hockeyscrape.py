326f9b4522a36e7ad664ad43dba9cd867d4b0300
Chih Chen <chc455@ucsd.edu>
2019-10-21 17:01:01 -0700

    Added .wait() to selenium scraping and fixed XPATH for web elements. Need to fix NaN/Null issues in 'home' and 'win'.

--- hockeyscrape.py
+++ hockeyscrape.py
@@ -4,31 +4,38 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
 from sqlalchemy import create_engine
-from sqlalchemy.types import BIGINT
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
 
-USE_SELEN = False
+
+USE_SELEN = True
 DATABASE = 'mysql+pymysql://user:pass@hockey-1.cgk9rffkqlbn.us-east-1.rds.amazonaws.com/hockey?charset=utf8'
 BASE_URL = 'https://www.hockey-reference.com/friv/dailyleaders.fcgi?month='
+
 
 def chrome_scrape(urlstring, date):
     driver = webdriver.Chrome('../chromedriver')
     driver.get(urlstring)
 
-    share_btn = driver.find_element_by_class_name('hasmore')
+    share_btn = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, "//*[contains(text(),'Share & more')]")))
     share_btn.click()
-    csv_btn = driver.find_element_by_xpath("//*[contains(text(),'Get table as CSV')]")
+
+    csv_btn = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, "//*[contains(text(),'Get table as CSV')]")))
+
+    # csv_btn = driver.find_element_by_xpath(
+    #     "//*[contains(text(),'Get table as CSV')]")
     csv_btn.click()
     csv = driver.find_element_by_id('csv_skaters').text
+    driver.quit()
 
-    df = pd.read_csv(pd.compat.StringIO(csv), header=1)   # change header
+    df = pd.read_csv(pd.compat.StringIO(csv), header=1, names=['rank', 'player', 'position', 'team', 'home', 'opp', 'win', 'boxscore', 'goals', 'assists', 'points', '+/-', 'pim', 'ev_goals',
+                                                               'pp_goals', 'sh_goals', 'gw_goals', 'ev_assists', 'pp_assists', 'sh_assists', 'sog', 'shoot%', 'shifts', 'toi', 'hit', 'blk', 'fow', 'fol', 'fo%'])
+    df['player'] = df['player'].str.split('\\').str[0]
+    df.insert(loc=0, column='date', value='20' + date[2] + '-' + date[0] + '-' + date[1])
 
     return clean(df)
-
-    # Rename the columns
-    # Drop boxscore
-    # Re Map
-    # Convert type
 
 
 def soup(urlstring, date):
@@ -46,19 +53,19 @@ def soup(urlstring, date):
 
     records = []
     for player in players:
-        stats = ['20' + date[2] + '-' + date[1] + '-' + date[1]]
+        stats = ['20' + date[2] + '-' + date[0] + '-' + date[1]]
         for data in player:
             stats.append(data.getText().split('\n')[0])
         records.append(stats)
-    df = pd.DataFrame(data, columns=['date', 'rank', 'player', 'position', 'team', 'home', 'opp', 'win', 'boxscore', 'goals', 'assists', 'points', '+/-', 'pim', 'ev_goals',
-                                     'pp_goals', 'sh_goals', 'gw_goals', 'ev_assists', 'pp_assists', 'sh_assists', 'sog', 'shoot%', 'shifts', 'toi', 'hit', 'blk', 'fow', 'fol', 'fo%'])
+    df = pd.DataFrame(records, columns=['date', 'rank', 'player', 'position', 'team', 'home', 'opp', 'win', 'boxscore', 'goals', 'assists', 'points', '+/-', 'pim', 'ev_goals',
+                                        'pp_goals', 'sh_goals', 'gw_goals', 'ev_assists', 'pp_assists', 'sh_assists', 'sog', 'shoot%', 'shifts', 'toi', 'hit', 'blk', 'fow', 'fol', 'fo%'])
     return clean(df)
 
 
-def clean_df(df):
+def clean(df):
     df.drop(columns=['boxscore'], inplace=True)
 
-    homemap = {'@': 0, '': 1, 'W': 1, 'L': 0}
+    homemap = {'@': 0, '': 1, 'W': 1, 'L': 0, 'L-OT': 0}
     df['home'] = df['home'].map(homemap)
     df['win'] = df['win'].map(homemap)
     df.iloc[:, 7:23] = df.iloc[:, 7:23].apply(pd.to_numeric)
@@ -67,9 +74,10 @@ def clean_df(df):
     df['rank'] = pd.to_numeric(df['rank'])
 
     df[['blk', 'hit', 'sog']] = df[['blk', 'hit', 'sog']].fillna(0)
-    df['total'] = df.apply(lambda row: 3 * row['goals'] + 2 * (row['assists'] + row['sh_assists'] + row['sh_goals']) + .3 * (row['+/-'] + row['sog'] + row['hit']) + .5 * (row['pim'] + row['pp_goals'] + row['pp_assists'] + row['blk']), axis=1)
+    df['total'] = df.apply(lambda row: 3 * row['goals'] + 2 * (row['assists'] + row['sh_assists'] + row['sh_goals']) + .3 * (
+        row['+/-'] + row['sog'] + row['hit']) + .5 * (row['pim'] + row['pp_goals'] + row['pp_assists'] + row['blk']), axis=1)
 
-    insert_db(df)
+    insert_db(df, table='daily')
 
 
 def insert_db(df, table, exists='append', db=DATABASE):
@@ -100,22 +108,14 @@ def main():
     log.close()
 
     x = datetime.datetime.today() - datetime.timedelta(1)
-    date = x.strftime('%x').split('/')
+    entry_date = x.strftime('%x').split('/')
 
-    #To do
-    try:
-        elif len(sys.argv) == 4:
-            date = sys.argv[1:]
-        else:
-        print('Bad input format')
-        log = open('log.txt', 'a')
-        log.write('Ban input format\nProgram aborted')
-        log.close()
+    if len(sys.argv) == 4:
+        entry_date = sys.argv[1:]
 
-    urlstring = BASE_URL + date[0] + "&day=" + date[1] + "&year=20" + date[2]
-    df = chrome_scrape(urlstring, date) if USE_SELEN else soup(urlstring, date)
-    insert_db(df, table='daily')
-
+    urlstring = BASE_URL + entry_date[0] + "&day=" + \
+        entry_date[1] + "&year=20" + entry_date[2]
+    chrome_scrape(urlstring, entry_date) if USE_SELEN else soup(urlstring, entry_date)
 
     log = open('log.txt', 'a')
     log.write('\n' + str(datetime.datetime.today()) +
